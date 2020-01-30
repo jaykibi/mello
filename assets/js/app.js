ab149631db14a9e5e106672ffedd0c360c182eb0
Raquibbi Muhammaddi <raquibbi@gmail.com>
2020-01-30 06:20:34 +0000

    finished lesson 12

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,18 @@ function handleListCreate() {
   }
 }
 
+function handleCardCreate(event) {
+  var $listContainer = event.target.parentNode;
+  var listId = Number($listContainer.getAttribute('data-id'));
+
+  var cardText = prompt('new card text') || '';
+
+  if (cardText.trim()) {
+    board.addCard(listId, cardText);
+    renderBoard();
+  }
+}
+ 
 
 function renderBoard() {
   $boardContainer.innerHTML = '';
@@ -18,14 +30,34 @@ function renderBoard() {
   board.lists.forEach(function(list) {
     var $listContainer = document.createElement('div');
     $listContainer.className = 'list';
+    $listContainer.setAttribute('data-id', list.id);
 
     var $header = document.createElement('header');
 
     var $headerButton = document.createElement('button');
     $headerButton.textContent = list.title;
 
+    var $cardUl = document.createElement('ul');
+
+    list.cards.forEach(function(card) {
+      var $cardLi = document.createElement('li');
+
+      var $cardButton = document.createElement('button');
+
+      $cardButton.textContent = card.text;
+
+      $cardLi.appendChild($cardButton);
+      $cardUl.appendChild($cardLi);
+    })
+
+    var $addCardButton = document.createElement('button');
+    $addCardButton.textContent = 'Add a card...';
+    $addCardButton.addEventListener('click', handleCardCreate);
+
     $header.appendChild($headerButton);
     $listContainer.appendChild($header);
+    $listContainer.appendChild($cardUl);
+    $listContainer.appendChild($addCardButton);
     $boardContainer.appendChild($listContainer);
   });
 
@@ -33,7 +65,7 @@ function renderBoard() {
   $addListContainer.className = 'list add';
 
   var $addListButton = document.createElement('button');
-  $addListButton.textContent = '+ 😂Add another list😂';
+  $addListButton.textContent = '+ Add another list';
   $addListButton.addEventListener('click', handleListCreate);
 
   $addListContainer.appendChild($addListButton);
--- assets/js/board.js
+++ assets/js/board.js
@@ -1,12 +1,16 @@
 class Card {
 	constructor(text) {
 		this.text = text;
+		this.id = ++Card.lastId;
 	}
 }
+
+Card.lastId = 0;
 
 class List {
 	constructor(title) {
 		this.title = title;
+		this.id = ++List.lastId;
 		this.cards = [];
 	}
 
@@ -16,8 +20,11 @@ class List {
 	}
 }
 
+List.lastId = 0;
+
 class Board {
 	constructor() {
+		this.id = ++Board.lastId;
 		this.lists = [];
 	}
 
@@ -25,7 +32,17 @@ class Board {
 		var list = new List(text);
 		this.lists.push(list);
 	}
+
+	addCard(listId, cardText) {
+		this.lists.forEach(function(list) {
+			if (listId === list.id) {
+				list.addCard(cardText);
+			}
+		});
+	}
 }
+
+Board.lastId = 0;
 
 var list = new List('my first list');
 list.addCard('My first card');
